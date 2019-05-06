c593fd4c7c31bc2ed4a5f09c6e46fce648c6c1ab
Nathan Patnam <patnamnv@email.wofford.edu>
2019-05-06 14:32:21 -0400

    file push

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import filedialog
+from tkinter import Canvas, filedialog, StringVar, Radiobutton
 from tkinter import font as tkfont
 
 
@@ -37,7 +37,6 @@ class SampleApp(tk.Tk):
         frame = self.frames[page_name]
         frame.tkraise()
 
-
 class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
@@ -63,17 +62,51 @@ class CreateDFA(tk.Frame):
         self.controller = controller
         self.add_title(controller)
         self.add_upload_dfa_from_file_button()
+        self.add_dfa_buttons(controller)
+        self.add_canvas(controller)
         self.add_return_home_button()
 
     def add_title(self, controller):
         label = tk.Label(self, text="Create a DFA",
                          font=controller.title_font)
         label.pack(side="top", fill="x", pady=10)
+    
+    def add_dfa_buttons(self, controller):
+        MODES = [
+            ("Add State", "ADD_STATE" , self.set_add_state_mode),
+            ("Add Transition Arrow", "ADD_TRANSITION", self.set_add_transition_mode),
+            ("Delete State", "DELETE_STATE", self.set_delete_mode),
+            ("Edit State", "EDIT_STATE", self.set_edit_state_mode),
+        ]
+        v = StringVar()
+        v.set("L")  # initialize
+
+        for text, mode, command in MODES:
+            b = Radiobutton(controller, text=text,
+                            variable=v, value=mode, command=command)
+            b.pack(anchor="w")
+    
+    def set_add_state_mode(self):
+        print("1")
+    
+    def set_add_transition_mode(self):
+        print("2")
+    
+    def set_delete_mode(self):
+        print("3")
+    
+    def set_edit_state_mode(self):
+        print("4")
 
     def add_upload_dfa_from_file_button(self):
         file_button = tk.Button(self, text="Create DFA from file",
                                 command=self.ask_for_file)
         file_button.pack()
+    
+    def add_canvas(self, controller):
+        canvas = Canvas(controller)
+
+
 
     def add_return_home_button(self):
         return_home_button = tk.Button(self, text="Go to the home page",
